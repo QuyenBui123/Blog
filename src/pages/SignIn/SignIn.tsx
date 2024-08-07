b5d73ea4c98c54ff4b6e51161a9fedad1e6649d5
QuyenBui123 <bvquyen.20it3@vku.udn.vn>
2024-08-07 21:12:17 +0700

    create sign up and sign in page

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,49 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen mt-20' >
+      <div className=' flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+        <Link 
+        to='/'
+        className='font-bold dark:text-white text-4xl'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Sahand's
+        </span>
+        Blog
+      </Link>
+      <p className='text-sm mt-5'>This is a demo project. You can sign with your email and password or with Google.</p>
+        </div>
+        {/* right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div className=''>
+              <Label value='Your username' />
+              <TextInput type='text ' placeholder='Username' id='username'/>
+            </div>
+            <div className=''>
+              <Label value='Your email' />
+              <TextInput type='text ' placeholder='Email' id='email'/>
+            </div>
+            <div className=''>
+              <Label value='Your Password' />
+              <TextInput type='text ' placeholder='Password' id='password'/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>
+              Sign Up
+            </Button>
+          </form>
+          <div>
+            <span>Have an account?</span>
+            <Link to='/SignIn'>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
