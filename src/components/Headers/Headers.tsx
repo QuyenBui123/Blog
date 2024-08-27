50b74b5c8a90d5868e6e8c0605bdaf67383875ac
QuyenBui123 <bvquyen.20it3@vku.udn.vn>
2024-08-27 11:03:04 +0700

    Feat(UI): Update layout Header

--- src/components/Headers/Headers.tsx
+++ src/components/Headers/Headers.tsx
@@ -1,134 +1,109 @@
-import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
-// import { FaSearch, FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
-import { AiOutlineSearch } from "react-icons/ai";
-// import { FaSun, FaMoon } from "react-icons/fa";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Avatar, Button, Dropdown } from "flowbite-react";
 import { useSelector } from "react-redux";
-// import { toggleTheme } from "../../redux/theme/ThemeSlice";
 import { RootState } from "../../redux/store";
-import SearchBar from "../Search/Search";
+import { FaBars, FaTimes } from "react-icons/fa"; // Import icons
+import SearchBar from "./Search";
 
-export interface UserState {
-  currentUser: {
-    username: string;
-    email: string;
-    profilePicture: string;
-  } | null;
-}
-export interface ThemeState {
-  theme: "light" | "dark";
-}
-export default function Header() {
-  // const dispatch = useDispatch();
-  const navigate = useNavigate();
-  // const { theme } = useSelector((state: RootState) => state.theme);
+const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
 
-  const handleNavigateHome = () => {
-    navigate("/");
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
 
-  const handleNavigateCreatPost = () => {
-    navigate("/Pos");
-  };
-  const handleNavigatePost = () => {
-    navigate("/blog");
-  };
-
-  const handleSearch = (query: string) => {
-    console.log("Tìm kiếm:", query);
-  };
   return (
-    <Navbar fluid rounded className=" lg:mx-20 ">
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Meta
-        </span>
-        Blog
-      </Link>
-      {/* <form>
-        <TextInput
-          type="text"
-          placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden lg:inline"
-        />
-      </form> */}
-      <SearchBar onSearch={handleSearch} />
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
-     <div className=" justify-end flex items-center">
-     <div className="flex gap-2 md:order-2">
-        {/* <Button
-          className='w-12 h-10 hidden sm:inline'
-          color='gray'
-          pill
-          onClick={() => dispatch(toggleTheme())}
-          >
-          {theme === 'light' ? <FaSun /> : <FaMoon />}
-          
-        </Button> */}
-        {currentUser ? (
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar alt="user" img={currentUser.profilePicture} rounded />
-            }
-          >
-            <Dropdown.Header>
-              <Link
-                to="/ProfilePage"
-                className=" hover:text-blue-500 flex justify-center items-center "
-              >
-                <Avatar
-                  alt="user"
-                  img={currentUser.profilePicture}
-                  size="sm"
-                  rounded
-                />
-                <span className="block text-sm ml-2 ">
-                  {currentUser.username}
-                </span>
-              </Link>
-            </Dropdown.Header>
-            <Link to={"/dashboard?tab=profile"}>
-              <Dropdown.Item>Profile</Dropdown.Item>
-            </Link>
-            <Dropdown.Divider />
-            <Dropdown.Item href="/">Sign out</Dropdown.Item>
-          </Dropdown>
-        ) : (
-          <Link to="/SignIn">
-            <Button gradientDuoTone="purpleToBlue" pill>
-              Sign In
-            </Button>
-          </Link>
-        )}
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse className=" mr-4">
-        {" "}
-        {/*  className='md:block ' */}
-        <Navbar.Link onClick={handleNavigateHome} className="cursor-pointer items-center ">
-          Home
-        </Navbar.Link>
-        <Navbar.Link
-          onClick={handleNavigateCreatPost}
-          className="cursor-pointer "
-        >
-          Create Post
-        </Navbar.Link>
-        <Navbar.Link onClick={handleNavigatePost} className="cursor-pointer ">
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        {/* Logo */}
+        <Link to="/" className="text-xl font-bold text-gray-800">
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Meta
+          </span>
           Blog
-        </Navbar.Link>
-      </Navbar.Collapse>
-     </div>
-    </Navbar>
-    
+        </Link>
+
+        {/* Search Bar */}
+        <div className="flex-1 mx-4 ">
+          <SearchBar onSearch={(query) => console.log("Tìm kiếm:", query)} />
+        </div>
+        <div className="md:hidden flex items-center mr-2">
+          <button
+            onClick={toggleMenu}
+            className="text-gray-600 hover:text-gray-800"
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}{" "}
+            {/* Toggle between open/close icons */}
+          </button>
+        </div>
+        {/* Navigation Links */}
+        <nav
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } md:flex items-center space-x-6 mr-4 bg-white md:bg-transparent absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto py-4 md:py-0 shadow-md md:shadow-none`}
+        >
+          <Link
+            to="/"
+            className="text-gray-600 hover:text-gray-800 block md:inline"
+          >
+            Home
+          </Link>
+          <Link
+            to="/blog"
+            className="text-gray-600 hover:text-gray-800 block md:inline"
+          >
+            Blog
+          </Link>
+          <Link
+            to="/pos"
+            className="text-gray-600 hover:text-gray-800 block md:inline"
+          >
+            Create Post
+          </Link>
+        </nav>
+
+        <div className=" md:flex items-center">
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar alt="user" img={currentUser.profilePicture} rounded />
+              }
+            >
+              <Dropdown.Header>
+                <Link
+                  to="/profile"
+                  className="hover:text-blue-500 flex justify-center items-center"
+                >
+                  <Avatar
+                    alt="user"
+                    img={currentUser.profilePicture}
+                    size="sm"
+                    rounded
+                  />
+                  <span className="block text-sm ml-2">
+                    {currentUser.username}
+                  </span>
+                </Link>
+              </Dropdown.Header>
+              <Dropdown.Item as={Link} to="/dashboard?tab=profile">
+                Profile
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="/">Sign out</Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to="/signIn">
+              <Button gradientMonochrome="purple">Sign In</Button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
   );
-}
+};
+
+export default Header;
---
+++ src/components/Headers/Search.tsx
@@ -0,0 +1,49 @@
+// SearchBar.tsx
+import React, { useState } from "react";
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="relative max-w-lg  ">
+      <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Tìm kiếm..."
+        className="w-full max-w-lg px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      />
+      <button type="submit" className="absolute right-0 top-0 mt-2 mr-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M11 4a7 7 0 107 7 7 7 0 00-7-7zM21 21l-4.35-4.35"
+          />
+        </svg>
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
