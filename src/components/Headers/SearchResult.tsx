690f0f6fc4eee4a4b6367962d4070f64125aa6f8
QuyenBui123 <bvquyen.20it3@vku.udn.vn>
2024-08-28 09:53:07 +0700

    Fix: Update profile route path in navigation

--- src/components/Headers/SearchResult.tsx
+++ src/components/Headers/SearchResult.tsx
@@ -1,36 +1,36 @@
-import { PostSearchItem } from '@/types'
-import { Avatar, List, Space, Typography } from 'antd'
-import { forwardRef } from 'react'
-import { twMerge } from 'tailwind-merge'
-import { Link } from 'react-router-dom'
+// import { PostSearchItem } from '@/types'
+// import { Avatar, List, Space, Typography } from 'antd'
+// import { forwardRef } from 'react'
+// import { twMerge } from 'tailwind-merge'
+// import { Link } from 'react-router-dom'
 
-type Props = {
-  className?: string
-  result: PostSearchItem[]
-  onMenuClick?: () => void
-}
+// type Props = {
+//   className?: string
+//   result: PostSearchItem[]
+//   onMenuClick?: () => void
+// }
 
-const SearchResult = forwardRef<HTMLDivElement, Props>(({ className, result, onMenuClick }, ref) => {
-  return (
-    <Space direction='vertical' className={twMerge('w-full max-h-96 overflow-y-scroll', className)} ref={ref}>
-      <Typography.Text className='font-semibold'>Search Result</Typography.Text>
-      <List
-        dataSource={result}
-        renderItem={(item) => (
-          <List.Item className='hover:bg-slate-100 rounded-md'>
-            <Link to="/" onClick={onMenuClick} className='w-full'>
-              <List.Item.Meta
-                className='w-full'
-                title={item.title}
-                description={limitTextToWords(item.shortContent, 10)}
-                avatar={<Avatar size='large' src={item.author.avatar} alt={item.author.name} />}
-              />
-            </Link>
-          </List.Item>
-        )}
-        rowKey='id'
-      />
-    </Space>
-  )
-})
-export { SearchResult }
+// const SearchResult = forwardRef<HTMLDivElement, Props>(({ className, result, onMenuClick }, ref) => {
+//   return (
+//     <Space direction='vertical' className={twMerge('w-full max-h-96 overflow-y-scroll', className)} ref={ref}>
+//       <Typography.Text className='font-semibold'>Search Result</Typography.Text>
+//       <List
+//         dataSource={result}
+//         renderItem={(item) => (
+//           <List.Item className='hover:bg-slate-100 rounded-md'>
+//             <Link to="/" onClick={onMenuClick} className='w-full'>
+//               <List.Item.Meta
+//                 className='w-full'
+//                 title={item.title}
+//                 description={limitTextToWords(item.shortContent, 10)}
+//                 avatar={<Avatar size='large' src={item.author.avatar} alt={item.author.name} />}
+//               />
+//             </Link>
+//           </List.Item>
+//         )}
+//         rowKey='id'
+//       />
+//     </Space>
+//   )
+// })
+// export { SearchResult }
